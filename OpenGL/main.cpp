e212bd2030a6e8bb59b2490c7e5e0b6b8de8cf8b
BenBR <benrichards6431@hotmail.com>
2023-12-28 17:49:41 +0000

    Camera Movement
    + smooth mouse and keyboard controls
    (late commit)

--- OpenGL/main.cpp
+++ OpenGL/main.cpp
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include "headers/shader.h"
 #include "headers/stb_image.h"
+#include "headers/camera.h"
 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
@@ -11,11 +12,21 @@
 
 // Function prototypes
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
-void processInput(GLFWwindow* window);
+void processInput(GLFWwindow* window, float deltatime);
+void mouseCallback(GLFWwindow* window, double xpos, double ypos);
+
+
+// Object Initialisation
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+
+bool firstMouse = true;
 
 #pragma region Settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
+
+float lastX = SCR_WIDTH / 2.0f;
+float lastY = SCR_HEIGHT / 2.0f;
 #pragma endregion
 
 
@@ -49,6 +60,9 @@ int main()
 
     Shader ourShader("shaders/vertex_shader.vs", "shaders/fragment_shader.fs");
 
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouseCallback);
+
 #pragma region Texture
     unsigned int texture;
     glGenTextures(1, &texture);
@@ -75,7 +89,6 @@ int main()
     stbi_image_free(data);
 #pragma endregion Texture
 
-#pragma region Vertices
 #pragma region Vertices
     float vertices[] = {
         // positions          // colors           // texture coords
@@ -121,7 +134,6 @@ int main()
         -0.5f,  0.5f,  0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 0.0f,
         -0.5f,  0.5f, -0.5f,  1.0f, 1.0f, 0.0f,  0.0f, 1.0f
     };
-#pragma endregion Vertices
 
     glm::vec3 cubePositions[] = {
         glm::vec3(0.0f,  0.0f,  0.0f),
@@ -135,6 +147,15 @@ int main()
         glm::vec3(1.5f,  0.2f, -1.5f),
         glm::vec3(-1.3f,  1.0f, -1.5f)
     };
+    const int numCubes = 10;
+    // Create an array of model matrices
+    glm::mat4 modelMatrices[numCubes];
+    for (int i = 0; i < numCubes; ++i) {
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, cubePositions[i]);
+        // Add rotation here if needed
+        modelMatrices[i] = model;
+    }
 #pragma endregion Vertices
 
 #pragma region Vertex Buffer Object (VBO)
@@ -142,59 +163,84 @@ int main()
     glGenBuffers(1, &VBO);  // Generate buffer and assign ID to VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);  // Bind VBO to GL_ARRAY_BUFFER
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);  // Load vertex data into buffer
+
+    unsigned int instanceVBO;
+    glGenBuffers(1, &instanceVBO);
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(glm::mat4) * numCubes, &modelMatrices[0], GL_STATIC_DRAW);
+
 #pragma endregion Vertex Buffer Object (VBO)
 
 #pragma region Vertex Array Object (VAO)
     unsigned int VAO;
     glGenVertexArrays(1, &VAO);  // Generate vertex array and assign ID to VAO
     glBindVertexArray(VAO);  // Bind VAO
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0); // Position attribute
+
+    // Bind the VBO of the cube vertices
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float))); // Color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
-    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float))); // Texture coords attribute
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
     glEnableVertexAttribArray(2);
 #pragma endregion Vertex Array Object (VAO)
 
+    // Bind the instance VBO
+    glBindBuffer(GL_ARRAY_BUFFER, instanceVBO);
+    // Set attribute pointers for matrix (4 times vec4)
+    for (unsigned int i = 0; i < 4; i++) {
+        glVertexAttribPointer(3 + i, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(float) * i * 4));
+        glEnableVertexAttribArray(3 + i);
+        glVertexAttribDivisor(3 + i, 1); // Tell OpenGL this is an instanced vertex attribute.
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
 #pragma region Element Buffer Object (EBO)
+    /*
     unsigned int EBO;
     glGenBuffers(1, &EBO);
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(cubePositions), cubePositions, GL_STATIC_DRAW);
+    */
 #pragma endregion Element Buffer Object (EBO)
 
 #pragma region Render Loop
     // Render loop
     while (!glfwWindowShouldClose(window))
     {
-        processInput(window);
+        // Calculate delta time for smooth camera movement
+        static float lastFrame = 0.0f;
+        float currentFrame = glfwGetTime();
+        float deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        // Input
+        processInput(window, deltaTime);
+
+        // Rendering commands here
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        // Camera/view transformation
+        glm::mat4 view = camera.getViewMatrix();
+        glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom),
+            (float)SCR_WIDTH / (float)SCR_HEIGHT,
+            0.1f, 100.0f);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Clear depth buffer too
 
-        // Create transformation matrices
-        glm::mat4 model = glm::mat4(1.0f);
-        glm::mat4 view = glm::lookAt(glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-        glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-
-        // Rotate the cube over time
-        model = glm::rotate(model, (float)glfwGetTime(), glm::vec3(0.5f, 1.0f, 0.0f));
-
-        // Apply transformations
         ourShader.use();
-        unsigned int modelLoc = glGetUniformLocation(ourShader.ID, "model");
         unsigned int viewLoc = glGetUniformLocation(ourShader.ID, "view");
         unsigned int projLoc = glGetUniformLocation(ourShader.ID, "projection");
 
-        glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
         glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
         glUniformMatrix4fv(projLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
         glBindTexture(GL_TEXTURE_2D, texture);
-        // Draw the cube
         glBindVertexArray(VAO);
-        glDrawArrays(GL_TRIANGLES, 0, 36); // 36 vertices for the 6 faces of the cube
+        glDrawArraysInstanced(GL_TRIANGLES, 0, 36, numCubes);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
@@ -204,7 +250,8 @@ int main()
 #pragma region Clean up
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteBuffers(1, &EBO);
+    glDeleteBuffers(1, &instanceVBO);
+    //glDeleteBuffers(1, &EBO);
 #pragma endregion
 
     glfwTerminate();
@@ -217,8 +264,38 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void processInput(GLFWwindow* window)
+// Update the processInput function to handle camera movement
+void processInput(GLFWwindow* window, float deltaTime)
 {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
         glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        camera.processKeyboard(FORWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        camera.processKeyboard(BACKWARD, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        camera.processKeyboard(LEFT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        camera.processKeyboard(RIGHT, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
+        camera.processKeyboard(UP, deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
+        camera.processKeyboard(DOWN, deltaTime);
 }
+
+    void mouseCallback(GLFWwindow * window, double xpos, double ypos) {
+        if (firstMouse) {
+            lastX = xpos;
+            lastY = ypos;
+            firstMouse = false;
+        }
+
+        float xoffset = xpos - lastX;
+        float yoffset = lastY - ypos; // Reversed since y-coordinates range from bottom to top
+        lastX = xpos;
+        lastY = ypos;
+
+        camera.processMouseMovement(xoffset, yoffset);
+    }
+
--- OpenGL/shaders/vertex_shader.vs
+++ OpenGL/shaders/vertex_shader.vs
@@ -2,19 +2,17 @@
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
 layout (location = 2) in vec2 aTexCoord;
+layout (location = 3) in mat4 instanceMatrix;
+
+uniform mat4 view;
+uniform mat4 projection;
 
 out vec3 ourColor;
 out vec2 TexCoord;
 
-// Uniforms for model, view, and projection matrices
-uniform mat4 model;
-uniform mat4 view;
-uniform mat4 projection;
-
 void main()
 {
-    // Apply the model, view, and projection matrices
-    gl_Position = projection * view * model * vec4(aPos, 1.0);
+    gl_Position = projection * view * instanceMatrix * vec4(aPos, 1.0f);
     ourColor = aColor;
     TexCoord = aTexCoord;
 }
